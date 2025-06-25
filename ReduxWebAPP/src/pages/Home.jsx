6fa86b3ef36b3798b5a2a42c348f7049f39030d9
penseurtank <ravi.rsinghvaishali@gmail.com>
2025-06-26 04:16:08 +0530

    home page title modification

--- ReduxWebAPP/src/pages/Home.jsx
+++ ReduxWebAPP/src/pages/Home.jsx
@@ -4,9 +4,8 @@ import Products from '../components/Products';
 const Home = () => {
     return (
         <div>
-            <h2 className="heading">Welcome to the Redux toolkit store</h2>
+            <h2 className="heading">Welcome to online store!</h2>
             <section>
-                <h3>Products</h3>
                 <Products />
             </section>
         </div>
