44c8043154e5cd65a14986a6b804a5b007be7720
penseurtank <ravi.rsinghvaishali@gmail.com>
2025-06-26 04:09:58 +0530

    product api fetched and mapping with good ui experience

--- ReduxWebAPP/src/App.jsx
+++ ReduxWebAPP/src/App.jsx
@@ -1,23 +1,55 @@
-// import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from 'react-router-dom';
 import Home from './pages/Home'
 import Cart from './pages/Cart'
 import Navbar from './components/Navbar'
+import './App.css';
+
+// Layout component that includes Navbar
+function Layout() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div><hr style={{ width: '100%', textAlign: 'left', marginLeft: 0 }} /></div>
+      <Outlet />
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'cart',
+        element: <Cart />
+      },
+    ]
+  }
+]);
 
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <div><hr style={{ width: '100%', textAlign: 'left', marginLeft: 0 }} /></div>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/cart' element={<Cart />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+    <RouterProvider
+      router={router}
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_skipActionErrorRevalidation: true,
+      }}
+    />
+  );
 }
 
-export default App
+export default App;
--- ReduxWebAPP/src/components/Products.jsx
+++ ReduxWebAPP/src/components/Products.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+
+import React, { useState, useEffect } from 'react'
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        const fetchProducts = async () => {
+            try {
+                const res = await fetch('https://fakestoreapi.com/products', {
+                    signal: controller.signal
+                });
+                const data = await res.json();
+                console.log(data);
+                setProducts(data);
+            } catch (error) {
+                if (error.name !== 'AbortError') {
+                    console.error('Fetch error:', error);
+                }
+            }
+        }
+
+        fetchProducts();
+
+        return () => {
+            controller.abort(); // Cleanup function
+        }
+    }, [])
+
+    const handleClick = () => {
+        console.log('add to cart action triggered...')
+    }
+
     return (
-        <div>Products</div>
+        <div className="productsWrapper">
+            {products.map((product) => (
+                <div className="card" key={product.id}>
+                    <div className="imgWrapper">
+                        <img src={product.image} alt="product_img" />
+                    </div>
+                    <h4 className="title">{product.title}</h4>
+                    <h5 className="price">${product.price}</h5>
+                    <div className="spacer" />
+                    <button className="btn" onClick={handleClick}>Add to cart</button>
+                </div>
+            ))}
+        </div>
+
+
     )
 }
 
